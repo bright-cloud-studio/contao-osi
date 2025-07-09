e6eb2a2bbe1ba69cdf15201548deb69878dee888
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 14:11:18 -0400

    Create config.php

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+use Contao\ArrayUtil;
+use Contao\System;
+
+/* Back end modules - Data DCAs */
+$GLOBALS['TL_LANG']['MOD']['osi'][0] = "Tests";
+$GLOBALS['BE_MOD']['osi']['test'] = array( 'tables' => array('tl_district') );
