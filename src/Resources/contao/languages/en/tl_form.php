fd6b102aeb233110290f12363f7d5f703bc71974
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 15:29:31 -0400

    Create tl_form.php

---
+++ src/Resources/contao/languages/en/tl_form.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_form']['type_legend'] = 'Form Type Settings';
+$GLOBALS['TL_LANG']['tl_form']['formType'] = ['Form Type', 'If this Form is a Test, select \'Test\'. Otherwise, leave as \'Default\''];
