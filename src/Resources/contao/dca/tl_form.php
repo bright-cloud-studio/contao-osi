90f6f5806188d78a6f986bfa12eea31b9c68feb1
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 15:45:39 -0400

    Update tl_form.php

--- src/Resources/contao/dca/tl_form.php
+++ src/Resources/contao/dca/tl_form.php
@@ -54,26 +54,3 @@ $GLOBALS['TL_DCA']['tl_form']['config']['onload_callback'][] = function (DataCon
         ->newsType;
 };
 
-
-$GLOBALS['TL_DCA']['tl_form_field']['fields']['type']['options_callback'] 	= array('tl_test_field', 'getFields');
-
-
-class tl_test_field extends tl_form_field
-{
-	public function getFields()
-	{
-		$fields = array();
-		$security = System::getContainer()->get('security.helper');
-
-		foreach ($GLOBALS['TL_FFL'] as $k=>$v)
-		{
-			if ($security->isGranted(ContaoCorePermissions::USER_CAN_ACCESS_FIELD_TYPE, $k))
-			{
-				$fields[] = $k;
-			}
-		}
-
-		return $fields;
-	}
-}
-
