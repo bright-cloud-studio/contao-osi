322f93e34e8576a0afcc651d9d0a96a62b319f88
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 13:33:06 -0400

    Create BcsOSIBundle.php

---
+++ src/BcsOSIBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao OSI
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-OSI
+ */
+
+namespace Bcs\OSIBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BcsOSIBundle extends Bundle
+{
+}
