a0291bae4259ce96eefed39cf24b70c80b86ad68
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 15:17:19 -0400

    Update and rename tl_test.php to tl_form.php

---
+++ src/Resources/contao/dca/tl_form.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Chart
+ * @license    LGPL-3.0+
+ * @see        https://github.com/bright-cloud-studio/dixon-inspiration
+ */
+
+use Contao\DataContainer;
+use Contao\Database;
+use Contao\Input;
+use Contao\System;
+
+// Insert the newsType field into the palette at an appropriate spot.
+$GLOBALS['TL_DCA']['tl_form']['palettes']['default'] = str_replace(
+    '{title_legend}',
+    '{type_legend},formType;{title_legend}',
+    $GLOBALS['TL_DCA']['tl_form']['palettes']['default']
+);
+
+// Append 'newsType' to the existing __selector__ array rather than overwriting it.
+$GLOBALS['TL_DCA']['tl_form']['palettes']['__selector__'][] = 'formType';
+
+// Define subpalettes for the various newsType options
+$GLOBALS['TL_DCA']['tl_form']['subpalettes']['formType_test'] = 'stepImage, stepDownload, stepVideo, stepDixonMaterials, stepOtherMaterials, stepObjectives';
+$GLOBALS['TL_DCA']['tl_form']['subpalettes']['formType_default'] = '';
+
+
+// Fields for the 'newsType' selection
+$GLOBALS['TL_DCA']['tl_form']['fields']['formType'] = array(
+    'label'     => &$GLOBALS['TL_LANG']['tl_form']['formType'],
+    'inputType' => 'select',
+    'options'   => array(
+        'test' => 'Test',
+        'default' => 'Default'
+    ),
+    'eval'      => array('submitOnChange' => true, 'mandatory' => true, 'tl_class' => 'w50'),
+    'default'   => 'default',
+    'sql'       => "varchar(255) NULL default 'default'"
+);
+
+// Ensure newsType dropdown appears when editing old content
+$GLOBALS['TL_DCA']['tl_form']['config']['onload_callback'][] = function (DataContainer $dc) {
+    if (!$dc->id) {
+        return;
+    }
+
+    $newsType = Database::getInstance()
+        ->prepare("SELECT formType FROM tl_form WHERE id=?")
+        ->execute($dc->id)
+        ->newsType;
+};
