9b21c6dadce90bee9b011118f4bbbe2d53435a41
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-09 14:20:58 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -5,4 +5,4 @@ use Contao\System;
 
 /* Back end modules - Data DCAs */
 $GLOBALS['TL_LANG']['MOD']['osi'][0] = "Tests";
-$GLOBALS['BE_MOD']['osi']['test'] = array( 'tables' => array('tl_district') );
+$GLOBALS['BE_MOD']['osi']['test'] = array( 'tables' => array('tl_test') );
