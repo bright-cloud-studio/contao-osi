f30e4614b64bbdd301cd004a64fbf98cd45c0ed7
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-13 19:11:57 -0400

    Update MultiChoiceWizard.php

--- src/Resources/contao/widgets/MultiChoiceWizard.php
+++ src/Resources/contao/widgets/MultiChoiceWizard.php
@@ -20,7 +20,7 @@ class MultiChoiceWizard extends OptionWizard
 			$this->varValue = array(array(''));
 		}
 
-		return System::getContainer()->get('twig')->render('@Contao/backend/widget/multi_choice_wizard.html.twig', array(
+		return System::getContainer()->get('twig')->render('@BcsOSI/multi_choice_wizard.html.twig', array(
 			'id' => $this->strId,
 			'rows' => $this->varValue,
 		));
