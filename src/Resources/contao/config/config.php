944d00674722607e82868cf39d28123aecfbfc6f
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-13 18:48:28 -0400

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,9 @@
 <?php
 
 use Bcs\OSIBundle\FormMultipleChoiceQuestion;
+use Bcs\MultiChoiceWizard;
 
 // Setup our Form field
 $GLOBALS['TL_FFL']['multiple_choice_question'] = 'Bcs\OSIBundle\FormMultipleChoiceQuestion';
+
+$GLOBALS['BE_FFL']['multi_choice_wizard'] = 'Bcs\MultiChoiceWizard';
