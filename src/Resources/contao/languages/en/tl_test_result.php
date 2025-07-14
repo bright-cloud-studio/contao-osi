be252c6887a4f95dfef282540465c885839876d6
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-14 18:23:45 -0400

    Create tl_test_result.php

---
+++ src/Resources/contao/languages/en/tl_test_result.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Contao Directory - Users can make submissions to a directory with a module to display and filter the results.
+ *
+ * Copyright (C) 2022 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/contao-directory
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+ */
+
+
+/* System Buttons */
+$GLOBALS['TL_LANG']['tl_test_result']['new']                        = array('New record', 'Add a new record');
+$GLOBALS['TL_LANG']['tl_test_result']['show']                       = array('Record details', 'Show the details of record ID %s');
+$GLOBALS['TL_LANG']['tl_test_result']['edit']                       = array('Edit record', 'Edit record ID %s');
+$GLOBALS['TL_LANG']['tl_test_result']['copy']                       = array('Copy record', 'Copy record ID %s');
+$GLOBALS['TL_LANG']['tl_test_result']['delete']                     = array('Delete record', 'Delete record ID %s');
+$GLOBALS['TL_LANG']['tl_test_result']['toggle']                     = array('Toggle record', 'Toggle record ID %s');
+
+/* System Fields */
+$GLOBALS['TL_LANG']['tl_test_result']['publish_legend']             = 'Publish';
+$GLOBALS['TL_LANG']['tl_test_result']['published']                  = array('Published', 'Show this record on the front end.');
+
+$GLOBALS['TL_LANG']['tl_test_result']['test_legend']    = 'Practice Details';
+$GLOBALS['TL_LANG']['tl_test_result']['test']                   = array('Test', 'ID of the Test');
+$GLOBALS['TL_LANG']['tl_test_result']['member']              = array('Member', 'ID of the Member who took the Test');
+$GLOBALS['TL_LANG']['tl_test_result']['submission_date']              = array('Submission Date', 'Date this submission was made');
+$GLOBALS['TL_LANG']['tl_test_result']['answers']              = array('Answers', 'Saved Answers');
+$GLOBALS['TL_LANG']['tl_test_result']['result_total_correct']           = array('Total Correct Answers', 'Count of the number of correct Answers');
+$GLOBALS['TL_LANG']['tl_test_result']['result_percentage']              = array('Total Percentage', 'Percentage of correct Answers');
