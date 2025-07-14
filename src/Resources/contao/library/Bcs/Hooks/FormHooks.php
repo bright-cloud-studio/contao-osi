a952d1bcd4b7dc78d5f0a43cc1b7bf6436c00401
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-14 19:43:32 -0400

    Update FormHooks.php

--- src/Resources/contao/library/Bcs/Hooks/FormHooks.php
+++ src/Resources/contao/library/Bcs/Hooks/FormHooks.php
@@ -2,9 +2,7 @@
 
 namespace Bcs\Hooks;
 
-use Bcs\Model\PoolModel;
-use Bcs\Model\PoolVariant;
-use Bcs\Model\QuoteRecord;
+use Bcs\Model\TestResult;
 
 use Contao\Controller;
 use Contao\Environment;
@@ -19,10 +17,23 @@ use DateTime;
 class FormHooks
 {
 
-    public function onSubmitTest($submittedData, $formData, $files, $labels, $form)
+    public function onSubmitTest($submittedData, $formData, $files, $labels, $test)
     {
-        if($form['formType'] == 'test') {
+        if($test->formType == 'test') {
             echo "Test Submitted";
+            
+            // Get Member
+            $member = FrontendUser::getInstance();
+
+            // Create Test Result record
+            $test_result = new TestResult();
+            $test_result->tstamp = time();
+            $test_result->submission_date = time();
+            $test_result->answers = json_encode($submittedData);
+            $test_result->member = $member->id;
+            $test_result->test = $test->id;
+            $test_result->save();
+            
             die();
         }
     }
