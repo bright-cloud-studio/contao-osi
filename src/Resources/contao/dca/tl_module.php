752bf89552a0787986162a716b24049b49900702
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-15 19:56:20 -0400

    Create tl_module.php

---
+++ src/Resources/contao/dca/tl_module.php
@@ -0,0 +1,3 @@
+<?php
+
+$GLOBALS['TL_DCA']['tl_module']['palettes']['mod_test_results']        = '{title_legend},name,headline,type;{template_legend:hide},customTpl;{expert_legend:hide},guests,cssID,space';
