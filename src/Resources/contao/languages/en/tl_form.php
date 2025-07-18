b32a648a381518b6aa5950317ef67881051f4d1f
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-18 10:29:53 -0400

    Update tl_form.php

--- src/Resources/contao/languages/en/tl_form.php
+++ src/Resources/contao/languages/en/tl_form.php
@@ -7,3 +7,7 @@ $GLOBALS['TL_LANG']['tl_form']['formType'] = ['Form Type', 'If this Form is a Te
 $GLOBALS['TL_LANG']['tl_form']['test_content_legend'] = 'Test Page Content';
 $GLOBALS['TL_LANG']['tl_form']['embed_code'] = ['Embed Code', 'Enter the embed code for the slider that displays on the Test Reader page'];
 $GLOBALS['TL_LANG']['tl_form']['additional_info'] = ['Additional Information', 'Enter any additional information to display on the Test Reader page'];
+
+
+$GLOBALS['TL_LANG']['tl_form']['member_group_legend'] = 'Member Group Assignment';
+$GLOBALS['TL_LANG']['tl_form']['member_groups'] = ['Member Groups', 'Select the Member Groups that will have access to this Test'];
