cd5094c5dffe1fd5db830a490f463a1bc02195f5
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-07-18 10:10:02 -0400

    Update tl_form.php

--- src/Resources/contao/languages/en/tl_form.php
+++ src/Resources/contao/languages/en/tl_form.php
@@ -2,3 +2,8 @@
 
 $GLOBALS['TL_LANG']['tl_form']['type_legend'] = 'Form Type Settings';
 $GLOBALS['TL_LANG']['tl_form']['formType'] = ['Form Type', 'If this Form is a Test, select \'Test\'. Otherwise, leave as \'Default\''];
+
+
+$GLOBALS['TL_LANG']['tl_form']['test_content_legend'] = 'Test Page Content';
+$GLOBALS['TL_LANG']['tl_form']['embed_code'] = ['Embed Code', 'Enter the embed code for the slider that displays on the Test Reader page'];
+$GLOBALS['TL_LANG']['tl_form']['additional_info'] = ['Additional Information', 'Enter any additional information to display on the Test Reader page'];
