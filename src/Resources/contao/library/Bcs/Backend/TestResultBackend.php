98c0639575331a125c9efc4c7a0dd10c1bdcc789
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-05 13:15:59 -0400

    Update TestResultBackend.php

--- src/Resources/contao/library/Bcs/Backend/TestResultBackend.php
+++ src/Resources/contao/library/Bcs/Backend/TestResultBackend.php
@@ -8,11 +8,29 @@ use Contao\FormModel;
 use Contao\Image;
 use Contao\Input;
 use Contao\MemberModel;
+use Contao\MemberGroupModel;
 use Contao\StringUtil;
 use Bcs\Model\TestResult;
 
 class TestResultBackend extends Backend
 {
+
+    // Get Members as options for a Select DCA field
+    public function getMemberGroups(DataContainer $dc) { 
+
+        $member_groups = array();
+
+		$this->import('Database');
+		$result = $this->Database->prepare("SELECT * FROM tl_member_group WHERE disable=0 ORDER BY name ASC")->execute();
+		while($result->next())
+		{
+            // Add ti array with ID as the value and firstname lastname as the label
+            $member_groups = $member_groups + array($result->id => $result->name);   
+		}
+
+		return $member_groups;
+	}
+    
     // Get Members as options for a Select DCA field
     public function getMembers(DataContainer $dc) { 
 
