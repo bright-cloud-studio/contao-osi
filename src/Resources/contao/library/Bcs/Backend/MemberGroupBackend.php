6eed3f545355bd73dac722ecc87536a02a6c473f
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-08-05 13:37:58 -0400

    Update MemberGroupBackend.php

--- src/Resources/contao/library/Bcs/Backend/MemberGroupBackend.php
+++ src/Resources/contao/library/Bcs/Backend/MemberGroupBackend.php
@@ -10,12 +10,38 @@ use Contao\MemberGroupModel;
 use Contao\MemberModel;
 use Contao\StringUtil;
 
+use Bcs\Model\TestResult;
+
 class MemberGroupBackend extends Backend
 {
 
     public function saveCallback($varValue, DataContainer $dc) {
-        echo "Die!";
-        die();
+
+        // Get the Test IDs
+        $group_ids = unserialize($varValue);
+
+        // Foreach Test
+        foreach($group_ids as $id) {
+            
+            // Get any Test Results that are assigned to this Test
+            $test_results = TestResult::findBy(['test = ?'], [$id]);
+            if($test_results) {
+                foreach($test_results as $test_result) {
+                    
+                    // Get any current Member Group values as an array
+                    $mem_groups = unserialize($test_result->member_groups);
+                    // Add our current Member Group to the list of selected Member Groups
+                    $mem_groups[] = $dc->activeRecord->id;
+                    // Save our changes
+                    $test_result->member_groups = serialize($mem_groups);
+                    $test_result->save();
+                }
+            }
+            
+        }
+
+
+        return $varValue;
     }
     
     // Get Members as options for a Select DCA field
