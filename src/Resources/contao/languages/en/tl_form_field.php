45313c8bb76c78754da6c0bbff915979fe4472b8
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-09-09 12:51:20 -0400

    Update tl_form_field.php

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -1,3 +1,4 @@
 <?php
 
 $GLOBALS['TL_LANG']['FFL']['multiple_choice_question'][0] = "Multiple Choice Question";
+$GLOBALS['TL_LANG']['FFL']['multiple_choice_image_question'][0] = "Multiple Choice Image Question";
