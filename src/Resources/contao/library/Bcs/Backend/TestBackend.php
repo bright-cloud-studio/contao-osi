5bff911e327bcb2f0a527213a98506fc7e1057b4
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-30 09:23:04 -0400

    Update TestBackend.php

--- src/Resources/contao/library/Bcs/Backend/TestBackend.php
+++ src/Resources/contao/library/Bcs/Backend/TestBackend.php
@@ -17,10 +17,28 @@ class TestBackend extends Backend
 
         // Hold the psys
         $training_images = array();
-
-        $training_images = $training_images + array('image_1' => 'Image One');
-        $training_images = $training_images + array('image_2' => 'Image Two');
-        
+        $training_images = $training_images + array('biohazard_waste' => 'Biohazard Waste');
+        $training_images = $training_images + array('climbing_ladder' => 'Climbing Ladder');
+        $training_images = $training_images + array('danger_labels' => 'Danger Labels');
+        $training_images = $training_images + array('fire_alarm' => 'Fire Alarm');
+        $training_images = $training_images + array('fire_extinguisher' => 'Fire Extinguisher');
+        $training_images = $training_images + array('forklift_and_cone' => 'Forklift and Cone');
+        $training_images = $training_images + array('hazmat_suit' => 'Hazmat Suit');
+        $training_images = $training_images + array('hydration_break' => 'Hydration Break');
+        $training_images = $training_images + array('keycard_entry' => 'Keycard Entry');
+        $training_images = $training_images + array('lab_mouse' => 'Lab Mouse');
+        $training_images = $training_images + array('laser_optics' => 'Laser Optics');
+        $training_images = $training_images + array('monkey' => 'Monkey');
+        $training_images = $training_images + array('office_argument' => 'Office Arguement');
+        $training_images = $training_images + array('pallet_jack' => 'Pallet Jack');
+        $training_images = $training_images + array('seatbelt' => 'Seatbelt');
+        $training_images = $training_images + array('spine_pain' => 'Spine Pain');
+        $training_images = $training_images + array('sterile_lab' => 'Sterile Lab');
+        $training_images = $training_images + array('toxic_chemical_label' => 'Toxic Chemical Label');
+        $training_images = $training_images + array('virus_safety' => 'Virus Safety');
+        $training_images = $training_images + array('weighing_powder' => 'Weighing Powder');
+        $training_images = $training_images + array('work_safety' => 'Work Safety');
+        $training_images = $training_images + array('worker_safety_symbols' => 'Worker Safety Symbols');
 		return $training_images;
 	}
     
