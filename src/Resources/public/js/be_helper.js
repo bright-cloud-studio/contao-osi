3626da23746d54ada4b33c7762b080fc111146d9
Bright Cloud Studio <mark@brightcloudstudio.com>
2025-10-30 09:55:24 -0400

    Refactor training image handling in be_helper.js

--- src/Resources/public/js/be_helper.js
+++ src/Resources/public/js/be_helper.js
@@ -1,50 +1,33 @@
-
 // When the page is loaded
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("PAGE LOADED");
-    
     // Get Training Image element
     var training_image = document.getElementById("ctrl_training_image");
     var to_append = document.getElementById("pal_certificate_legend");
     
     // If we have a value, display the image
     if(training_image.value) {
-        
+        // Remove a previous Training Image
         removeOldTrainingImage();
-        var image_url = getTrainingImageUrl(training_image.value);
+        // Assemble our image's URL
+        var image_url = '/files/content/certificate_images/' + training_image.value + '.jpeg';
+        // Append HTML containing our Training Image so the user can preview it
         to_append.insertAdjacentHTML("afterbegin", "<div id='training_image_helper' class='clr widget' style='padding-top:5px;'><img id='hotspot_image' src='" + image_url + "' width='300px'></div>");
-        
     }
-    
     
     const selectElement = document.getElementById("ctrl_training_image");
     selectElement.addEventListener("change", function() {
         
+        // Remove a previous Training Image
         removeOldTrainingImage();
-        var image_url = getTrainingImageUrl(training_image.value);
+        // Assemble our image's URL
+        var image_url = '/files/content/certificate_images/' + training_image.value + '.jpeg';
+        // Append HTML containing our Training Image so the user can preview it
         to_append.insertAdjacentHTML("afterbegin", "<div id='training_image_helper' class='clr widget' style='padding-top:5px;'><img id='hotspot_image' src='" + image_url + "' width='300px'></div>");
-        
     });
     
 });
 
-
-
-function getTrainingImageUrl(field_value) {
-    let image_url;
-    
-    switch (field_value) {
-        case 'image_1':
-          image_url = '/files/content/certificate_images/test_image_1.jpg'; // Root path for the homepage
-          break;
-        case 'image_2':
-          image_url = '/files/content/certificate_images/test_image_2.jpg';
-          break;
-      }
-    
-    return image_url;
-}
-
+// Attempts to find an already existing "#training_image_helper" and remove() it, so we only display one at a time
 function removeOldTrainingImage() {
     var deleteOld = document.getElementById("training_image_helper");
     if(deleteOld != null)
