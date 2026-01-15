579f54f6fbdd145fae23ffa935e692de53ef48bf
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-14 20:32:24 -0500

    Update PrepareFormDataHook.php

--- src/Hook/PrepareFormDataHook.php
+++ src/Hook/PrepareFormDataHook.php
@@ -8,17 +8,25 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsHook;
 use Contao\Form;
 use Contao\Widget;
 
+use Terminal42\NotificationCenterBundle\NotificationCenter;
+
 /**
  * Provides several function to access leads hooks and send notifications.
  */
 #[AsHook('prepareFormData')]
 class PrepareFormDataHook
 {
+    private $notificationCenter;
     /**
      * @param array<mixed>  $submittedData
      * @param array<mixed>  $labels
      * @param array<Widget> $fields
      */
+    public function __construct(NotificationCenter $notificationCenter)
+    {
+        $this->notificationCenter = $notificationCenter;
+    }
+    
     public function __invoke(array &$submittedData, array $labels, array $fields, Form $form): void
     {
         echo "FINALLY!";
