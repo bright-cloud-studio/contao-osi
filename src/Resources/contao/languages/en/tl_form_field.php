5e83b4ae018eac469f3253f227234d3b72cfddd8
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-21 18:08:41 -0500

    Add label image option to form field language file

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -2,3 +2,5 @@
 
 $GLOBALS['TL_LANG']['FFL']['multiple_choice_question'][0] = "Multiple Choice Question";
 $GLOBALS['TL_LANG']['FFL']['multiple_choice_image_question'][0] = "Multiple Choice Image Question";
+
+$GLOBALS['TL_LANG']['tl_form_field']['label_image'] = ['Label Image', 'Select an image to display below the Label and above the Questions'];
