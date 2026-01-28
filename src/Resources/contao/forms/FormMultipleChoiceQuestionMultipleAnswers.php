f5775ff17166bd972f6babd4a7f0a5d27873d7ac
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-28 14:18:00 -0500

    Rename class to FormMultipleChoiceQuestionMultipleAnswers

--- src/Resources/contao/forms/FormMultipleChoiceQuestionMultipleAnswers.php
+++ src/Resources/contao/forms/FormMultipleChoiceQuestionMultipleAnswers.php
@@ -6,7 +6,7 @@ use Contao\FormCheckbox;
 use Contao\Config;
 use Contao\FormModel;
 
-class FormMultipleChoiceQuestion extends FormCheckbox
+class FormMultipleChoiceQuestionMultipleAnswers extends FormCheckbox
 {
     protected $strTemplate = 'form_multi_choice_question_multiple_answers';
     protected $strPrefix = 'widget widget-checkbox widget-multi-choice-question-multiple-answers';
