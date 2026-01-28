bfcd719fd6187083ef8857df3810dd30ea0dc5ee
Bright Cloud Studio <mark@brightcloudstudio.com>
2026-01-28 14:04:51 -0500

    Add translation for multiple choice question with answers

--- src/Resources/contao/languages/en/tl_form_field.php
+++ src/Resources/contao/languages/en/tl_form_field.php
@@ -1,6 +1,7 @@
 <?php
 
 $GLOBALS['TL_LANG']['FFL']['multiple_choice_question'][0] = "Multiple Choice Question";
+$GLOBALS['TL_LANG']['FFL']['multiple_choice_question_multiple_answers'][0] = "Multiple Choice Question - Multiple Answers";
 $GLOBALS['TL_LANG']['FFL']['multiple_choice_image_question'][0] = "Multiple Choice Image Question";
 
 $GLOBALS['TL_LANG']['tl_form_field']['label_image'] = ['Label Image', 'Select an image to display below the Label and above the Questions'];
