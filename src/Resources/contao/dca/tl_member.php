6f3b3bae93dab21550724d9b168796e3bed08295
AMIT SAINI <amitsaini@AMITs-MacBook-Pro.local>
2026-02-18 12:00:37 +0530

    Implemented backend features -  Member Export and Added question number to questions in test.

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -4,3 +4,18 @@ use Contao\DataContainer;
 
 /* Modify default Member Group sorting to be alphabetical */
 $GLOBALS['TL_DCA']['tl_member']['fields']['groups']['flag'] = 2;
+
+/* Modify default Member listing sorting */
+$GLOBALS['TL_DCA']['tl_member']['list']['sorting']['fields'] = array('firstname ASC');
+
+/* Add Member Groups to the list view */
+$GLOBALS['TL_DCA']['tl_member']['list']['label']['fields'][] = 'groups';
+$GLOBALS['TL_DCA']['tl_member']['list']['label']['label_callback'] = array('Bcs\Backend\MemberBackend', 'generateLabel');
+/* Export Operation */
+$GLOBALS['TL_DCA']['tl_member']['list']['global_operations']['export_csv'] = array(
+    'label'      => array('Export CSV', 'Export current list to CSV'),
+    'href'       => 'key=export_csv',
+    'class'      => 'header_icon',
+    'attributes' => 'onclick="Backend.getScrollOffset()"',
+    'button_callback'     => ['Bcs\Backend\MemberBackend', 'exportCSVButton']
+);
